1663ed5cccffe0cf61d31c4b9487ebae0a4454f5
Owlspec3086 <ethical.worker3@gmail.com>
2020-07-18 00:48:33 -0400

    BaseUrl was spelt wrong BasweURL

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,53 +1,47 @@
-import React, { useState } from "react";
-//import (authWithAxios) and is useHistory
-import { axiosWithAuth } from "../utils/axiosWithAuth";
+import React, { useState } from 'react';
+import { axiosWithAuth } from '../utils/axiosWithAuth';
 import { useHistory } from 'react-router-dom';
-
 
 const Login = () => {
   const { push } = useHistory();
 
   const [credentials, setCredentials] = useState({
-    username: 'Lambda School',
+    username: '',
     password: '',
   });
+  // make a post request to retrieve a token from the api
+  // when you have handled the token, navigate to the BubblePage route
 
-  
   const handleChange = e => {
-    setCredentials({...credentials, [e.target.name]: e.target.value});
+    setCredentials({ ...credentials, [e.target.name]: e.target.value });
   };
-  
+
   const loginSubmit = e => {
     e.preventDefault();
-    // make a post request to retrieve a token from the api
     axiosWithAuth()
-    .post('/api/login', credentials)
-    .then(res => {
-      console.log(res);
-      
-      // when you have handled the token, navigate to the BubblePage route
-      localStorage.setItem('token', res.data.payload);
-      push('/bubbles');
-    })
-    .catch(err => console.log(err));
-
+      .post("http://localhost:5000/api/login", credentials)
+      .then(res => {
+        console.log(res);
+        localStorage.setItem('token', res.data.payload);
+        push('/bubbles');
+      })
+      .catch(err => console.log(err));
   };
 
   return (
     <form onSubmit={loginSubmit}>
       <h1>Welcome to the Bubble App!</h1>
       <input
-      type='text'
-      name='username'
-      value={credentials.username}
-      onChange={handleChange}
+        type='text'
+        name='username'
+        value={credentials.username}
+        onChange={handleChange}
       />
-
-      <input 
-      type='password'
-      name='password'
-      value={credentials.password}
-      onChange={handleChange}
+      <input
+        type='password'
+        name='password'
+        value={credentials.password}
+        onChange={handleChange}
       />
       <button>Login</button>
     </form>
